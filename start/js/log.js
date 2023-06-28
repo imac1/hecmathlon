a9309c8bdf278c50fa7d55a9a6fb9907f133626c
imac1 <irina.macovei@yahoo.fr>
2023-06-28 23:01:07 +0300

    tasks 1 & 2 ready

--- start/js/log.js
+++ start/js/log.js
@@ -1,35 +1,35 @@
-import hECMAthlon from "./main.js"
+import HECMAthlon from "./main.js"
 
 function init() {
-    var maxValueBtn = document.querySelector(".maxValue__btn");
-    var greaterThanBtn = document.querySelector(".greaterThan__btn");
-    var fizzBuzzBtn = document.querySelector(".fizzBuzz__btn");
+    const maxValueBtn = document.querySelector(".maxValue__btn");
+    const greaterThanBtn = document.querySelector(".greaterThan__btn");
+    const fizzBuzzBtn = document.querySelector(".fizzBuzz__btn");
 
-    var maxValueInput = document.querySelector(".maxValue__input");
-    var greaterThanInput = document.querySelector(".greaterThan__input");
-    var fizzBuzzInput = document.querySelector(".fizzBuzz__input");
+    const maxValueInput = document.querySelector(".maxValue__input");
+    const greaterThanInput = document.querySelector(".greaterThan__input");
+    const fizzBuzzInput = document.querySelector(".fizzBuzz__input");
 
-    var maxValueRoot = document.querySelector(".maxValue__container");
-    var greaterThanRoot = document.querySelector(".greaterThan__container");
-    var fizzBuzzRoot = document.querySelector(".fizzBuzz__container");
+    const maxValueRoot = document.querySelector(".maxValue__container");
+    const greaterThanRoot = document.querySelector(".greaterThan__container");
+    const fizzBuzzRoot = document.querySelector(".fizzBuzz__container");
 
     maxValueBtn.addEventListener('click', () => {
-        var maxValue = getOutput(hECMAthlon.getMaxValue(maxValueInput.value));
+        const maxValue = getOutput(HECMAthlon.getMaxValue(maxValueInput.value));
         logResult(maxValueRoot, maxValue);
     });
     greaterThanBtn.addEventListener('click', () => {
-        var greaterValues = getOutput(hECMAthlon.getGreaterThan(greaterThanInput.value));
+        const greaterValues = getOutput(HECMAthlon.getGreaterThan(greaterThanInput.value));
         logResult(greaterThanRoot, greaterValues);
     });
     fizzBuzzBtn.addEventListener('click', () => {
-        var result = getOutput(hECMAthlon.fizzBuzz(fizzBuzzInput.value));
+        const result = getOutput(HECMAthlon.fizzBuzz(fizzBuzzInput.value));
         logResult(fizzBuzzRoot, result);
     });
 }
 
 function getOutput(output) {
-    var returnValue = ["The function starts"];
-    for(var i = 0; i < output.length; i++){
+    const returnValue = ["The function starts"];
+    for(let i = 0; i < output.length; i++){
         returnValue.push(output[i]);
     }
     returnValue.push("The function ends");
@@ -40,7 +40,7 @@ function logResult(place, values){
     while(place.firstChild) {
         place.firstChild.remove();
     }
-    for (var j = 0; j < values.length; j++) {
+    for (let j = 0; j < values.length; j++) {
         place.insertAdjacentHTML("beforeend", "<div>" + values[j] + "</div>");
     }
 }
--- start/js/main.js
+++ start/js/main.js
@@ -1,32 +1,32 @@
-const hECMAthlon = {
+class HECMAthlon {
 
-    getMaxValue: function (input) {
-        var maxValues = input.split(",");
-        var maxValue = 0;
-        for(var i = 0; i < maxValues.length; i++){
-            var tempValue = parseInt(maxValues[i]);
+  static getMaxValue(input) {
+        let maxValues = input.split(",");
+        let maxValue = 0;
+        for(let i = 0; i < maxValues.length; i++){
+            const tempValue = parseInt(maxValues[i]);
             if (tempValue > maxValue) {
                 maxValue = tempValue;
             }
         }
         return [maxValue];
-    },
+    }
 
-    getGreaterThan: function (input) {
-        var greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        var greaterNums = [];
-        for(var j = 0; j < greaterValues.length; j++) {
+   static getGreaterThan (input) {
+        const greaterValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const greaterNums = [];
+        for(let j = 0; j < greaterValues.length; j++) {
             if (greaterValues[j] > parseInt(input)) {
                 greaterNums.push(greaterValues[j]);
             }
         }
         return greaterNums;
-    },
+    }
 
-    fizzBuzz: function (input) {
-        var output = [];
-        for (var k = 1; k <= parseInt(input); k++) {
-            var value;
+    static fizzBuzz (input) {
+        const output = [];
+        for (let k = 1; k <= parseInt(input); k++) {
+            let value;
             if(k % 5 === 0 && k % 3 === 0) value = "FizzBuzz";
             else if(k % 3 === 0) value = "Fizz";
             else if(k % 5 === 0) value = "Buzz";
@@ -36,5 +36,4 @@ const hECMAthlon = {
         return output;
     }
 }
-
-export default hECMAthlon;
+export default HECMAthlon;
